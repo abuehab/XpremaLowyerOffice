252a429aa7ae71e2a550a746f9b9474dbeca1171
Abu Ehab <MyGlassBox@Hotmail.Com>
2016-01-23 16:14:52 +0200

    Create Project Now

---
+++ XpremaLowyerOfficeSolution/Xprema.LowyerOffice.Datalayer/Tables Classes/EmployeeCmd.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xprema.LowyerOffice.Datalayer.Tables_Classes
+{
+    public class EmployeeCmd
+    {
+
+
+
+
+    }
+}
---
+++ XpremaLowyerOfficeSolution/Xprema.LowyerOffice.Interface/Reports/ReportsDisplayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xprema.LowyerOffice.Interface.Reports
+{
+    public class ReportsDisplayer
+    {
+
+
+    }
+}
